aa7554c121b795f2801ae5be15d4611e71e25b45
Riokyanpu <kgst1216@gmail.com>
2024-03-25 15:12:48 +0900

    First commit"

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :nickname, length: { maximum: 6 }
+  validates :surname, presence: true
+  validates :surname_kana, presence: true
+  validates :name, presence: true
+  validates :name_kana, presence: true
+  validates :date_of_birth , presence: true
+  
+end
